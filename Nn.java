4a2309f236e3e50670371fd9323439f3b732addf
Adwaith Shaju <adwaithshaju2003@gmail.com>
2025-01-30 11:57:56 +0530

    commit

--- Nn.java
+++ Nn.java
@@ -1,10 +1,11 @@
 import java.util.*;
 import java.io.*;
 import java.lang.Math;
+import java.lang.reflect.Array;
 
 class value{
 
-    ArrayList<value> toposot = new ArrayList<>();
+    ArrayList<value> topo = new ArrayList<>();
     ArrayList<value> prev = new ArrayList<>();
     ArrayList<value> visited = new ArrayList<>();
 
@@ -16,12 +17,10 @@ class value{
     value(double data){
         this.data=data;
     } 
-
     value(double data,ArrayList<value> children,String op){
         this.data=data;
         this.op=op;
         this.prev=children;
-
     }
     @Override
     public String toString(){
@@ -48,7 +47,41 @@ class value{
         value out= new value(this.data * v.data,children,"*");
         return out;
     }
+    value tanh(){
+        ArrayList<value> children = new ArrayList<>();
+        double a =this.data;
+        double res = (Math.exp(a*2)-1)/(Math.exp(a*2)+1);
+        children.add(this);
+        value out =new value(res,children,"tanh");
+        return out;
+    }
+    value pow(double v){
+        ArrayList<value> children = new ArrayList<>();
+        children.add(this);
+        children.add(new value(v));
+        value out = new value(Math.pow(this.data,v),children,"^");
+        return out;
+    }
 
+    void buildtopo(value v){
+        if(visited.contains(v)==false){
+            visited.add(v);
+            Iterator<value> it = v.prev.iterator();
+            while (it.hasNext()){
+                value child = it.next(); 
+                buildtopo(child);
+            }
+            topo.add(v);
+        }
+    }
+
+    void reverse(){
+        buildtopo(this);
+        for(int i=topo.size()-1;,i>-1;i--){
+            topo.get(i).backward();
+        }
+
+    }
 }
 class Nn{
     public static void main(String[] args){
