1a0296540ee178dcecdc506d4dda9be25dd25d87
Adwaith Shaju <adwaithshaju2003@gmail.com>
2025-01-30 11:37:10 +0530

    commit

--- Nn.java
+++ Nn.java
@@ -13,22 +13,46 @@ class value{
     String label="";
     String op="";
 
-    value(double data,String label){
+    value(double data){
         this.data=data;
-        this.label=label;
+    } 
+
+    value(double data,ArrayList<value> children,String op){
+        this.data=data;
+        this.op=op;
+        this.prev=children;
+
     }
     @Override
     public String toString(){
         return "Value(DATA)="+data;
     }
     value add(value v){
-        value out= new value(this.data+v.data,"add");
+        ArrayList<value> children = new ArrayList<>();
+        children.add(this);
+        children.add(v);
+        value out= new value(this.data + v.data,children,"+");
+        return out;
+    }
+    value sub(value v){
+        ArrayList<value> children = new ArrayList<>();
+        children.add(this);
+        children.add(v);
+        value out= new value(this.data - v.data,children,"-");
+        return out;
+    }
+    value mul(value v){
+        ArrayList<value> children = new ArrayList<>();
+        children.add(this);
+        children.add(v);
+        value out= new value(this.data * v.data,children,"*");
+        return out;
     }
 
 }
 class Nn{
     public static void main(String[] args){
-        value v1=new value(1.0,"v1");
+        value v1=new value(1.0);
         System.out.println(v1);
     }
 }
