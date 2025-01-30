d23948a5856438a7d919708909466c83828893be
Adwaith Shaju <adwaithshaju2003@gmail.com>
2025-01-30 10:50:04 +0530

    Third Commit

--- Nn.java
+++ Nn.java
@@ -3,13 +3,32 @@ import java.io.*;
 import java.lang.Math;
 
 class value{
+
+    ArrayList<value> toposot = new ArrayList<>();
+    ArrayList<value> prev = new ArrayList<>();
+    ArrayList<value> visited = new ArrayList<>();
+
     double grad=0.0;
     double data=0.0;
     String label="";
     String op="";
+
+    value(double data,String label){
+        this.data=data;
+        this.label=label;
+    }
+    @Override
+    public String toString(){
+        return "Value(DATA)="+data;
+    }
+    value add(value v){
+        value out= new value(this.data+v.data,"add");
+    }
+
 }
 class Nn{
     public static void main(String[] args){
-
+        value v1=new value(1.0,"v1");
+        System.out.println(v1);
     }
 }
